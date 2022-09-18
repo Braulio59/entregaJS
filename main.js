b6a12bcecc45b29a2abb1d4ef883ad881cd57793
Juane <juanehueda885@gmail.com>
2022-09-18 18:47:00 -0300

    nuevos archivos

--- main.js
+++ main.js
@@ -1,45 +1,52 @@
 const pizzaMenu = [
     {
-        id: 0, 
+        id: 1, 
         name: "Muzzarella", 
         ingredients: ["salsa de tomate","Queso Muzzarella", "Oregano","Aceituna"],
         price: 500,
+        img: "assets\\pizza-muzarella.png"
     }, 
     {
-        id: 1, 
+        id: 2, 
         name: "Rucula",
         ingredients: ["Salsa de tomate", "Queso Muzzarella,", "Oregano","Aceituna", "Rucula"],
         price : 800,
+        img: "assets\\pizza-rucula.png"
     },
     {
-        id:2,
+        id: 3,
         name:"Fugazzeta",
         ingredients: ["salsa de tomate","Queso Muzzarella","Cebolla", "Aceituna",],
         price: 600,
+        img:"assets\\Fugazzeta.png"
     },
     {
-        id:3, 
+        id: 4, 
         name:"Napolitana",
         ingredients: ["Salsas de tomate", "Queso Muzzarella","Tomate Fresco","Ajo"],
         price: 700,
+        img: "assets\\pizza-napo.png"
     },
     {
-        id:4, 
+        id: 5, 
         name: "Roquefort",
         ingredients: ["Salsa de tomate", "Queso Muzzarella","Roquefort", "Parmesano", "Fontina"],
         price: 900,
+        img: "assets\\Pizza-Roquefort.png"
     }, 
     {
-        id: 5,
+        id: 6,
         name: "Calabresa",
         ingredients:["Salsa de tomate","Queso Muzzarella","Aceituna","Salame"],
-        price: 800,
+        price: 800, 
+        img: "assets\\pizza-calabresa.png" 
     },
     {
-        id: 6,
+        id: 7,
         name: "Champignones",
         ingredients:["Salsa de tomate","Queso Muzzarella","Champignones","Hongos de pino"],
         price:  900,
+        img: "assets\\1624279230635.png"
     }
 ] 
 
@@ -50,7 +57,15 @@ const pizzaMenu = [
 const pizzaOrderForm = document.getElementById('pizzaOrderForm')
 const pizzaSelectionInput = document.getElementById('pizzaSelection')
 const pizzaName = document.getElementById("pizzaName")
-const pizzaPrice = document.getElementById("pizzaPrice")
+const pizzaPrice = document.getElementById("pizzaPrice") 
+const pizzaPhoto = document.getElementById("pizza_photo")
+const errorMessage = document.getElementById("errorMessage")
+const errorText = document.getElementById("error_mensaje") 
+
+const saveLocalStorage = () => {
+	localStorage.setItem('pizzaMenu', JSON.stringify(pizzaMenu));
+};
+
 
 const init = () => {
   pizzaOrderForm.addEventListener('submit', (event) => {
@@ -58,18 +73,38 @@ const init = () => {
     orderPizza(pizzaSelectionInput.value, pizzaMenu)
     
   })
+  pizzaSelectionInput.addEventListener("keydown",() => {
+    if(pizzaSelectionInput.value !== "") return
+    errorMessage.style.display = "none"
+  })
+  saveLocalStorage()
 } 
 
 const orderPizza = (pizzaId, pizzaList) => {
+    if(pizzaId === "") {
+        clearValues()
+        alert ("Por favor ingresa un número")
+        return
+    }
     let selectedPizza = pizzaList.find(f => pizzaId == f.id) 
     if (!selectedPizza) {  
-    pizzaName.textContent = null; 
-    pizzaPrice.textContent = null;
-    alert("La Pizza que desea ordenar no existe")
+    clearValues()
+    errorMessage.style.display = "flex"
+    errorText.textContent = `No se pudo encontrar la pizza ${pizzaId}`
     return
-    }
+}
+
     pizzaName.textContent = selectedPizza.name
     pizzaPrice.textContent = selectedPizza.price
+    pizzaPhoto.src = selectedPizza.img
+    pizzaPhoto.style.display = "flex"
+    errorMessage.style.display = "none"
+}
+
+const clearValues = () => {
+    pizzaName.textContent = null; 
+    pizzaPrice.textContent = null;
+    pizzaPhoto.style.display = "none"
 }
 
 init()
